a3934c37fcf280c12bc4bd1f5ae5ffea71a5fc1c
Yeison Alexis Velasco Trejos <yeison.velasco@caminos.com.co>
2024-01-23 19:22:24 -0500

    Se añade lista con los clientes(Prueba) #Mejorar CSS

--- src/app/components/prestamos/prestamos/prestamos.component.ts
+++ src/app/components/prestamos/prestamos/prestamos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-prestamos',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './prestamos.component.html',
   styleUrl: './prestamos.component.css'
 })
-export class PrestamosComponent {
+export class PrestamosComponent implements OnInit {
 
+  constructor( private http:HttpClient){}
+  prestamos :any
+  ngOnInit(): void {
+    this.getData().then(res=>{
+      this.prestamos = (res.results);
+      console.log(this.prestamos)
+    })
+  }
+
+  async getData(): Promise<any> {
+    return new Promise((resolve)=>{
+      const url = 'https://rickandmortyapi.com/api/character/?page=19';
+      this.http.get(url).subscribe((response)=>{
+
+        if(!response){
+          alert("Ha ocurrido un error")
+        }
+
+        resolve(response)
+      },async(error)=>{
+        resolve(error.error)
+      })
+    }).catch(error => {
+      // Imprimo el error
+      console.error(error);
+    });
+  }
 }
