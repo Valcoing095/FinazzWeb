51c561d4c8a100acb19541394c1205eb5793369c
caminos <yeison.velasco@caminos.com.co>
2024-12-17 08:35:33 -0500

    corrección

--- src/app/components/prestamos/crear-prestamo/crear-prestamo.component.ts
+++ src/app/components/prestamos/crear-prestamo/crear-prestamo.component.ts
@@ -18,26 +18,7 @@ export class CrearPrestamoComponent implements OnInit{
   ngOnInit(): void {
     this.getClients()
   }
-<<<<<<< HEAD
-
-  /** 
-   * Form to create loan
-   * @property {form} loanData - loanData.
-  */
-  formLoanData = new FormGroup({
-    id_cliente : new FormControl(),
-    valor_deuda: new FormControl(),
-    cuotas: new FormControl(),
-    fecha_inicio: new FormControl(),
-  });
-
-  /**
-  * Carga todos los clientes creados a una list
-  * @returns {clients} Todos los clientes creados 
-  */
-=======
   // Se crea formulario con los clientes ya ceados a los que se les va a realizar el prestamo
->>>>>>> dev-front
   getClients(){
     this.clientService.getClients().subscribe({
       next:(result)=>{
@@ -49,17 +30,8 @@ export class CrearPrestamoComponent implements OnInit{
     })
   }
 
-<<<<<<< HEAD
-
-  /**
-   * Crear prestamo a un cliente  
-   *  @returns {boolean} status  true or false 
-  */ 
-  createLoan(){
-=======
   createPrestamos(){
 
->>>>>>> dev-front
   }
 
 }
